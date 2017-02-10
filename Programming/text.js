a91ba6dd1614559865486e21bd27aacc8d12feee
manikantadbv <manikantadbv@gmail.com>
2017-02-10 12:42:08 +0530

    Updated To check

---
+++ Programming/text.js
@@ -0,0 +1,56 @@
+function out()
+	{
+		var x,y,p,q,z;
+			
+		
+		var s=0;
+		var temp=p;
+		while(p>0)
+		{
+			s = s+(p%10);
+			p = Math.floor(p/10);
+			var t=s;
+		}
+		
+		var b=0;
+		
+		if(t=>10)
+		{
+			while(t>0)
+			{
+				
+				b=b+(t%10);
+				t= Math.floor(t/10);
+				var u=b;
+			}
+		}
+		if(t<10)
+		{
+			if(t==q)
+			{
+				
+				
+			}
+			else
+			{
+				
+			}
+		}
+		
+		
+			if(u==q)
+			{
+				
+				
+			}
+			else
+			{
+						}
+				
+		
+		
+		
+		
+		
+	}
+	
